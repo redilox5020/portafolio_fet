b9ecc028397549aa9c9588a517c398e2a4aed549
redilox5020 <tec.tuto4@gmail.com>
2025-04-05 18:29:42 -0500

    vista 403 personalizada

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.dashboard_admin')
+@section('main')
+<!-- 404 Error Text -->
+<div class="text-center">
+    <div class="error mx-auto" data-text="403">403</div>
+    <p class="lead text-gray-800 mb-5">Pagina no disponible</p>
+    <p class="text-gray-500 mb-0">Esta acción no está autorizada.</p>
+    <a href="{{ route('inicio') }}">&larr; Ir al inicio</a>
+</div>
+@endsection
